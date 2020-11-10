1fb9a912f50e72e839b6b1da9240a497742b9d97
DEVIN G <devingomez1@hotmail.com>
2020-11-09 18:35:53 -0800

    Updated so you can add as many classess as you want
    -Self note
    • Finish Discord Webhooks
    • Finish writing Array crap starting at line 33

--- Zoom Auto-Join/Program.cs
+++ Zoom Auto-Join/Program.cs
@@ -7,6 +7,8 @@ using System.Net.Http;
 using System.Threading;
 using DiscordRPC;
 using DiscordRPC.Logging;
+
+// USE THIS
 using JNogueira.Discord.Webhook.Client;
 
 
@@ -14,54 +16,50 @@ namespace Zoom_Auto_Join
 {
     class Program
     {
+        /*
+          {
+            { "Physics", "12:00:00 AM", "google.com"  },
+            { "English", "12:01:00 AM", "youutube.com" },
+            { "Math", "12:03:00 AM", "foo.com" },
+          }
+         */
 
         public class Zoom
         {
             // Constructor that takes one argument:
-            public Zoom(string link, string time)
+            public Zoom(string[,] classess)
             {
-               Link = link;
-               Time = time;
+                
+                    Console.WriteLine(classess.Length);
+               
+                
             }
+
+            //var zoom = new Zoom(new string[] { new string[] { "Physics", "12:00:00 AM", "youtube.com" }, new string[] { "English", "12:01:00 AN", "google.com" } });
+
+
+            //string[] classess = { { "Period 1", "12:00:00 AM", "Google.com" }, { "Period 2", "12:01:00 AM", "youtube.com" } };
+            /*string[,] classess = new string[,]
+            { 
+                { "A", "AB", "AC" }, { "B", "BC", "BA" }, { "C", "CA", "CB" }
+            };*/
+
+
+
+
+
             
-            public void joinClassOnReady() {
 
-                //Console.WriteLine("This.time = " + this.Time + "\nCurrent Time = "+ DateTime.Now.ToString("HH:mm:ss"));
-                Console.WriteLine("Waiting to join class at " + Convert.ToDateTime(this.Time).ToString("HH:mm:ss") + " " + Convert.ToDateTime(this.Time).ToString("tt"));
-                var MyIni = new IniFile("settings.ini");
-                var joinSound = MyIni.Read("Join Sound", "Sound");
-                var soundsEnabled = Convert.ToBoolean(MyIni.Read("Mute Sounds", "Sound"));
-                while (true)
-                {
-                    if(this.Time == DateTime.Now.ToString("HH:mm:ss"))
-                    {
 
-                        
 
-                        if (string.IsNullOrEmpty(joinSound))
-                        {
-                            SoundPlayer audio = new SoundPlayer(Zoom_Auto_Join.Properties.Resources.join);
-                            if (!soundsEnabled) { audio.Play(); }
-                        }
-                        else
-                        {
-                            SoundPlayer audio = new SoundPlayer(joinSound);
-                            if (!soundsEnabled) { audio.Play(); }
-                        }
-
-                        //SoundPlayer audio = new SoundPlayer(Zoom_Auto_Join.Properties.Resources.intro);
-                        //if (!soundEnabled) { audio.Play(); }
-
-                        Process.Start("chrome.exe", this.Link);
-                        Console.WriteLine("Joining class link: " + Link);
-                        break;
-                    }
-                    Thread.Sleep(100);
-                }
+            public void joinClass(string link)
+            {
+                Process.Start("chrome.exe", link);
+                Console.WriteLine("Joining class link: " + link);
             }
-            
-            
-            
+
+
+
             // Auto-implemented readonly property:
             public string Link { get; }
             public string Time { get; }
@@ -71,12 +69,12 @@ namespace Zoom_Auto_Join
             public override string ToString()
             {
                 return "Current Link: " + Link + "\nCurrent Time: " + Time;
-                
+
             }
         }
 
 
-        
+
 
         // Discord ShiT
         public static readonly DiscordRpcClient client = new DiscordRpcClient("775260345597034526");
@@ -89,12 +87,12 @@ namespace Zoom_Auto_Join
             int randomTxtArr = rnd.Next(1, imagetextArr.Length + 1);
 
 
-            
+
             client.Initialize();
 
             client.SetPresence(new RichPresence()
             {
-                Details = "made by smashguns#6175",
+                Details = "By smashguns#6175",
                 //State = "csharp example",
                 Assets = new Assets()
                 {
@@ -124,12 +122,43 @@ namespace Zoom_Auto_Join
             var responseString = await response.Content.ReadAsStringAsync();
         }*/
 
+        public static string AmpmTo24(DateTime time)
+        {
+            return Convert.ToDateTime(time).ToString("HH:mm:ss");
+        }
+        public static string MilitaryToAmPm(DateTime time)
+        {
+            return Convert.ToDateTime(time).ToString("hh:mm:ss") + " " + Convert.ToDateTime(time).ToString("tt");
+        }
+
+
+
+
+
+
 
         static void Main(string[] args)
         {
+            /*
+            string[] links = { "google.com", "youtube.com" };
+            string[] periods = { "Physics": {"12:00:00AM", "google.com"}, "English": {"12:01:00AM", "youtube.com"}, "Math": {"12:02:00AM", "yahoo.com"}}
+            string[] times = { "12:00:00 AM", "12:01:00 AM"};
+            var zoom = new Zoom(links,times);*/
+
+            //string[] periods = { "Physics": {"12:00:00AM", "google.com"}, "English": {"12:01:00AM", "youtube.com"}, "Math": {"12:02:00AM", "yahoo.com"}}
+
+       
             DiscordStatus();
-            //TEtetetete
-            
+            var zoom = new Zoom
+                    (new string[,]
+                        {
+                            { "Physics", "12:00:00 AM", "google.com"  },
+                            { "English", "12:01:00 AM", "youutube.com" },
+                            { "Math", "12:03:00 AM", "foo.com" },
+                        }
+                    );
+
+
             if (!File.Exists("settings.ini"))
             {
                 var MyIni = new IniFile("settings.ini");
@@ -176,8 +205,6 @@ namespace Zoom_Auto_Join
                 var soundsEnabled = Convert.ToBoolean(MyIni.Read("Mute Sounds", "Sound"));
                 var introSound = MyIni.Read("Start Up Sound", "Sound");
 
-                Console.WriteLine("Mondayperiod1T = " + Mondayperiod1T);
-
 
                 if(string.IsNullOrEmpty(introSound))
                 {
@@ -196,7 +223,6 @@ namespace Zoom_Auto_Join
                 DateTime MondaytimeValue2 = Convert.ToDateTime(Mondayperiod2T);
                 DateTime MondaytimeValue3 = Convert.ToDateTime(Mondayperiod3T);
 
-                Console.WriteLine("MondaytimeValue1 = " + MondaytimeValue1.ToString("HH:mm:ss"));
 
                 DateTime timeValue1 = Convert.ToDateTime(period1T);
                 DateTime timeValue2 = Convert.ToDateTime(period2T);
@@ -221,11 +247,13 @@ namespace Zoom_Auto_Join
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.WriteLine("Made by smashguns#6175");
                 Console.ForegroundColor = ConsoleColor.White;
+                //new Zoom(new string[] { new string[] { "Physics", "12:00:00 AM", "youtube.com" }, new string[] { "English", "12:01:00 AN", "google.com" } });
                 
+
                 if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
                 {
                     Console.WriteLine("Today is monday");
-                    
+                    /*
                     var zoom1 = new Zoom(period1L, MondaytimeValue1.ToString("HH:mm:ss"));        
                     zoom1.joinClassOnReady();
                       
@@ -233,10 +261,11 @@ namespace Zoom_Auto_Join
                     zoom2.joinClassOnReady();
 
                     var zoom3 = new Zoom(period3L, MondaytimeValue3.ToString("HH:mm:ss"));
-                    zoom3.joinClassOnReady();
+                    zoom3.joinClassOnReady();*/
                 }
                 else
                 {
+                    /*
                     var zoom1 = new Zoom(period1L, timeValue1.ToString("HH:mm:ss"));
                     zoom1.joinClassOnReady();
                     
@@ -244,10 +273,12 @@ namespace Zoom_Auto_Join
                     zoom2.joinClassOnReady();
 
                     var zoom3 = new Zoom(period3L, timeValue3.ToString("HH:mm:ss"));
-                    zoom3.joinClassOnReady();
+                    zoom3.joinClassOnReady();*/
                 }
                 Console.WriteLine("Seems like classess ended");
             }
+            
+            
             client.UpdateEndTime();
             client.Dispose();
             Console.WriteLine("Press any key to continue");
